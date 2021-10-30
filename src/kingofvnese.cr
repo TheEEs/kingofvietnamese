9809ee1e81be7514cd8adb55642fa563f675023e
TheEEs <visualbasic2013@hotmail.com>
2021-10-30 18:53:06 +0700

    another version

--- src/kingofvnese.cr
+++ src/kingofvnese.cr
@@ -1,12 +1,27 @@
 require "./word_picker.cr"
 require "json"
 require "kemal"
+require "mime"
 require "./fs"
 
 wp = WordPicker.new "./dicts/dict.txt"
 
-serve_static false
-#public_folder "./frontface" 
+if Kemal.config.env == "production"
+  serve_static false
+  get "/*path" do |env|
+    env.response.content_type = MIME.from_filename(env.params.url["path"])
+    FileStorage.get?(env.params.url["path"]).try &.gets_to_end
+  end
+
+  get "/" do |env|
+    FileStorage.get?("index.html").try &.gets_to_end
+  end
+else
+  public_folder "./frontface"
+  get "/" do |env|
+    File.read("./frontface/index.html")
+  end
+end
 
 before_get "/word" do |env|
   env.response.content_type = "application/json"
@@ -14,15 +29,6 @@ end
 
 get "/word" do
   wp.pick_a_random_word.to_json
-end
-
-get "/*path" do |env|
-  print(env.params.url["path"])
-  FileStorage.get(env.params.url["path"]).gets_to_end
-end
-
-get "/" do |env|
-    FileStorage.get("index.html").gets_to_end 
 end
 
 Kemal.run
